1eb011d027bce57adefaa2422074e9dbd268120b
tuanflute275 <tuanflute275@gmail.com>
2023-08-09 17:29:13 +0700

    Feat: Making Home Page, Header, Footer

--- src/app/components/layouts/header/header.component.ts
+++ src/app/components/layouts/header/header.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  heart: any = 1;
+  logo = '../../../assets/images/logo.jpg';
+  avatar = '../../../assets/img/avatars/avatar_admin.jpg'
 
+  logout(){
+    
+  }
 }
